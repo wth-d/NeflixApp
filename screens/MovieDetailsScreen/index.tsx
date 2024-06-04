00b77f11eb0a25100752541437867036535c2700
Wei <111629334+wth-d@users.noreply.github.com>
2024-06-04 00:06:30 -0400

    Finished upper part of MovieDetailsScreen. (This part will become the FlatList header.)
    vd1 - hr2

--- screens/MovieDetailsScreen/index.tsx
+++ screens/MovieDetailsScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Image } from "react-native";
-import { MaterialIcons } from '@expo/vector-icons';
+import { Image, Pressable } from "react-native";
+import { MaterialIcons, Entypo, Octicons, AntDesign, Feather, FontAwesome } from '@expo/vector-icons';
 import { Text, View } from "@/components/Themed";
 import styles from "./styles";
 
@@ -11,20 +11,62 @@ const firstEpisode = movie.seasons.items[0].episodes.items[0];
 const MovieDetailsScreen = () => {
   return (
     <View style={styles.container}>
-      {/* <Text>MovieDetailsScreen is here</Text> */}
       <Image style={styles.image}
         source={{ uri: firstEpisode.poster }}
       />
-      <Text>{movie.title}</Text>
-      <View style={{ flexDirection: 'row' }}>
-        <Text style={styles.match}>98% match</Text>
-        <Text style={styles.year}>{movie.year}</Text>
-        <View style={styles.ageContainer}>
-          <Text style={styles.age}>12+</Text>
+      <View style={{ padding: 12 }}>
+        <Text style={styles.title}>{movie.title}</Text>
+        <View style={{ flexDirection: 'row', }}>
+          <Text style={styles.match}>98% match</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+          <View style={styles.ageContainer}>
+            <Text style={styles.age}>12+</Text>
+          </View>
+          <Text style={styles.year}>{movie.numberOfSeasons} Seasons</Text>
+          <MaterialIcons name="hd" size={24} color="white" />
         </View>
-        <Text style={styles.year}>{movie.numberOfSeasons} Seasons</Text>
-        <MaterialIcons name="hd" size={24} color="white" />
       </View>
+
+      {/* Play button */}
+      <Pressable onPress={() => { console.warn('play') }} style={styles.playButton}>
+        <Text style={styles.playButtonText}>
+          <Entypo name="controller-play" size={16} color="black" />
+          {' '}
+          Play
+        </Text>
+      </Pressable>
+
+      {/* Download button */}
+      <Pressable onPress={() => { console.warn('download') }} style={styles.downloadButton}>
+        <Text style={styles.downloadButtonText}>
+          <Octicons name="download" size={16} color="white" />
+          {'  '}
+          Download
+        </Text>
+      </Pressable>
+
+      <Text style={{ marginVertical: 10 }}>{movie.plot}</Text>
+      <Text style={styles.year}>Cast: {movie.cast}</Text>
+      <Text style={styles.year}>Creator: {movie.creator}</Text>
+
+      {/* Row with 3 icon buttons */}
+      <View style={{ flexDirection: 'row', marginTop: 20 }}>
+        <View style={{ alignItems: 'center', marginHorizontal: 20 }}>
+          <AntDesign name="plus" size={24} color="white" />
+          <Text style={{ color: 'darkgrey', marginTop: 5 }}>My List</Text>
+        </View>
+
+        <View style={{ alignItems: 'center', marginHorizontal: 20 }}>
+          <Feather name="thumbs-up" size={24} color="white" />
+          <Text style={{ color: 'darkgrey', marginTop: 5 }}>Rate</Text>
+        </View>
+
+        <View style={{ alignItems: 'center', marginHorizontal: 20 }}>
+          <FontAwesome name="send-o" size={24} color="white" />
+          <Text style={{ color: 'darkgrey', marginTop: 5 }}>Share</Text>
+        </View>
+      </View>
+
     </View>
   );
 };
