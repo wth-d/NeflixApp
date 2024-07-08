34b2a25ab5e105da6549149f3d52bb46d46bd27d
Wei <111629334+wth-d@users.noreply.github.com>
2024-07-08 03:45:47 -0400

    Add stack navigation between HomeScreen and MovieDetailsScreen;
    End of vd1

--- app/(tabs)/HomeTab.tsx
+++ app/(tabs)/HomeTab.tsx
@@ -1,4 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { Stack } from 'expo-router';
 import HomeScreen from "@/screens/HomeScreen";
 import MovieDetailsScreen from "@/screens/MovieDetailsScreen";
 
@@ -6,20 +7,28 @@ const HomeTabStack = createStackNavigator();
 // const HomeStack = createStackNavigator<HomeParamList>();
 
 const HomeTab = () => {
-  return (
+  // return (
+  //   <Stack>
+  //     <Stack.Screen name="HomeScreen" options={{ headerShown: false }} />
+  //     <Stack.Screen name="MovieDetailsScreen" options={{  }} />
+  //   </Stack>
+  // );
+
+  return ( // Note: the Screen at the top (in this Stack) is rendered first.
     <HomeTabStack.Navigator>
-      
+      <HomeTabStack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+
       <HomeTabStack.Screen
         name="MovieDetailsScreen"
         component={MovieDetailsScreen}
         options={{ title: 'some title', }}
         // options={{ headerShown: false }}
       />
-      <HomeTabStack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
+      
     </HomeTabStack.Navigator>
   );
 };
--- components/HomeCategory/index.tsx
+++ components/HomeCategory/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Image, FlatList } from "react-native";
+import { Image, FlatList, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { Text, View } from '@/components/Themed';
 import styles from "./styles";
@@ -16,6 +17,13 @@ interface HomeCategoryProps {
 }
 
 const HomeCategory = (props: HomeCategoryProps) => {
+  const nav = useNavigation();
+  const onMoviePress = (movie: { id: string, poster: string }) => {
+    // console.warn("movie is", movie);
+    nav.navigate("MovieDetailsScreen", { id: movie.id }); // typescript error to be fixed
+    // Will query the database to display the movie with this id.
+  };
+
   return (
     // or perhaps use <React.Fragment>
     <>
@@ -23,7 +31,9 @@ const HomeCategory = (props: HomeCategoryProps) => {
       <FlatList
         data={props.category.movies}
         renderItem={({ item }) => (
-          <Image style={styles.image} source={{ uri: item.poster }} />
+          <Pressable onPress={() => onMoviePress(item)}>
+            <Image style={styles.image} source={{ uri: item.poster }} />
+          </Pressable>
         )}
         horizontal
         // showsHorizontalScrollIndicator={false}
